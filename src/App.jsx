cc3ddf12309851f5aaf4a3fadc5939fd27fc4bdf
Pushkar Gupta <pushkardps8@gmail.com>
2024-09-16 15:45:28 +0530

    Implemented Browser Functionality related hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css'
+import { useOnline, useMousePointer} from './custom-hooks/BrowserFunctionality';
 import {useTodos} from "./custom-hooks/DataFetching"
 
 function App() {
   const {todos, loading} = useTodos(5);
+  const mousePointer = useMousePointer()
 
-  if(loading){
-    return <div>
-      ..loading
-    </div>
-  }
+  // const isOnline = useOnline()
+  
+
+  // if(loading){
+  //   return <div>
+  //     ..loading
+  //   </div>
+  // }
+
+  // if(isOnline) return "You are online"
+  
+  // return "You are offline"
 
   function Track({ todo }) {
     return <div>
@@ -18,8 +27,10 @@ function App() {
     </div>
   }
 
+
   return (
     <>
+      Your mouse position is {mousePointer.x} {mousePointer.y}
       {todos.map(todo => <Track todo={todo} />)}
     </>
   )
---
+++ src/custom-hooks/BrowserFunctionality.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export function useOnline(){
+    const [isOnline, setIsOnline] = useState(window.navigator.onLine)
+
+    useEffect(() => {
+        window.addEventListener("online", () => {
+            setIsOnline(true)
+        })
+
+        window.addEventListener("offline", () => {
+            setIsOnline(false)
+        })
+    },[])
+
+    return isOnline
+}
+
+export const useMousePointer = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return position;
+}
